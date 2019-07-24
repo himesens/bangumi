71f7cca149cf049a1bf7b02016e310f915d8a311
himesens <37564342+himesens@users.noreply.github.com>
2019-07-24 09:00:58 +0800

    Add files via upload

---
+++ src/main/java/himesens/bangumi/BangumiApplication.java
@@ -0,0 +1,13 @@
+package himesens.bangumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangumiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/himesens/bangumi/BangumiApplicationTests.java
@@ -0,0 +1,16 @@
+package himesens.bangumi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BangumiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
